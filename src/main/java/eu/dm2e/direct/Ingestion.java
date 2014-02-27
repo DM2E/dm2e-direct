c946cc5a0ea5f8a95fee52f067682b86e9661764
Konstantin Baierer <konstantin.baierer@gmail.com>
2014-02-27 14:47:07 +0100

    undebug

--- src/main/java/eu/dm2e/direct/Ingestion.java
+++ src/main/java/eu/dm2e/direct/Ingestion.java
@@ -556,12 +556,10 @@ public class Ingestion {
 		sb.append(		sw.toString());
 		sb.append("  }  \n");
 		sb.append("}  \n");
-//		sb.append("WHERE { } ");
-		log.info("UPDATE request '{}'", sb.toString());
-		log("UPDATE request " + sb.toString());
+//		log.info("UPDATE request '{}'", sb.toString());
+//		log("UPDATE request " + sb.toString());
 		UpdateRequest update = UpdateFactory.create();
 		update.add(sb.toString());
-		log("UPDATE Request: " + update.toString());
 		UpdateProcessor exec = UpdateExecutionFactory.createRemoteForm(update, endpointUpdate);
 		exec.execute();
 	}
