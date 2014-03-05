7beb56b0bfc36e9e3d3617785281fade89a2fe64
Konstantin Baierer <konstantin.baierer@gmail.com>
2014-03-05 14:06:48 +0100

    undebug

--- src/main/java/eu/dm2e/direct/Ingestion.java
+++ src/main/java/eu/dm2e/direct/Ingestion.java
@@ -572,7 +572,7 @@ public class Ingestion {
 		sb.append(		sw.toString());
 		sb.append("  }  \n");
 		sb.append("}  \n");
-		log.info("Update Query for file '{}' : \n '{}'", tmp, sb.toString());
+//		log.info("Update Query for file '{}' : \n '{}'", tmp, sb.toString());
 		UpdateRequest update = UpdateFactory.create();
 		update.add(sb.toString());
 //		log("UPDATE Request: " + update.toString());
